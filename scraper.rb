d34411af66ec2ae2275a28ff69ac35669d39c6c5
Tony Bowden <tony@tmtm.com>
2016-07-16 12:54:10 +0100

    Initial scraper

---
+++ scraper.rb
@@ -0,0 +1,70 @@
+#!/bin/env ruby
+# encoding: utf-8
+
+require 'colorize'
+require 'nokogiri'
+require 'open-uri'
+require 'scraperwiki'
+
+require 'pry'
+require 'open-uri/cached'
+OpenURI::Cache.cache_path = '.cache'
+
+class String
+  def tidy
+    self.gsub(/[[:space:]]+/, ' ').strip
+  end
+end
+
+class Page
+
+  def initialize(url)
+    @url = url
+  end
+
+  def as_data
+    @md ||= Hash[ protected_methods.map { |m| [m, send(m)] } ]
+  end
+
+  protected
+
+  def results
+    triples = results_box[results_box.find_index('Yaren') .. -2]
+    triples.each_slice(3).map do |area, winners, _|
+      { 
+        area: area,
+        winners: parse_winners(winners),
+      }
+    end
+  end
+
+  private
+
+  def noko
+    @noko ||= Nokogiri::HTML(open(@url).read)
+  end
+
+  def results_box
+    # I can't get the regexp right here to split on \n\s+\n, so this
+    # will have to do for now
+    @_results ||= noko.css('.articleContent p').text.gsub(/\n\s+/m, '----EO----').split('----EO----').map(&:tidy)
+  end
+
+  def parse_winners(str)
+    winner_rx = /(\w+ \w+) (\d+\.\d+)/
+    str.scan(winner_rx).map { |name, votes| { name: name, votes: votes.gsub('.','') } }
+  end
+
+
+
+end
+
+source = 'http://www.naurugov.nr/government-information-office/media-release/elected-members-for-nauru-elections-2016-(1).aspx'
+data = Page.new(source).as_data[:results].map do |r|
+  r[:winners].map do |w|
+    w.merge(area: r[:area])
+  end
+end.flatten
+
+ScraperWiki.save_sqlite([:name, :area], data)
+
