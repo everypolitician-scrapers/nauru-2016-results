6838407b27e8e121d23f32815e0b772aa7030bda
Tony Bowden <tony@tmtm.com>
2016-07-18 23:54:35 +0100

    Move to a NauruGov::ElectionResults class

--- scraper.rb
+++ scraper.rb
@@ -16,50 +16,54 @@ class String
   end
 end
 
-class Page
+class NauruGov
 
-  def initialize(url)
-    @url = url
-  end
+  class ElectionResults
 
-  def as_data
-    @md ||= Hash[ protected_methods.map { |m| [m, send(m)] } ]
-  end
-
-  protected
-
-  def results
-    triples = results_box[results_box.find_index('Yaren') .. -2]
-    triples.each_slice(3).map do |area, winners, _|
-      { 
-        area: area,
-        winners: parse_winners(winners),
-      }
+    def initialize(url)
+      @url = url
     end
+
+    def as_data
+      @md ||= Hash[ protected_methods.map { |m| [m, send(m)] } ]
+    end
+
+    protected
+
+    def results
+      triples = results_box[results_box.find_index('Yaren') .. -2]
+      triples.each_slice(3).map do |area, winners, _|
+        {
+          area: area,
+          winners: parse_winners(winners),
+        }
+      end
+    end
+
+
+    private
+
+    def noko
+      @noko ||= Nokogiri::HTML(open(@url).read)
+    end
+
+    def results_box
+      # I can't get the regexp right here to split on \n\s+\n, so this
+      # will have to do for now
+      @_results ||= noko.css('.articleContent p').text.gsub(/\n\s+/m, '----EO----').split('----EO----').map(&:tidy)
+    end
+
+    def parse_winners(str)
+      str.split(/(?:, | and )/).map { |s|
+        s.reverse.split(/\s+/,2).reverse.map(&:reverse)
+      }.map { |name, votes| { name: name, votes: votes.sub(/\.$/,'') } }
+    end
+
   end
-
-  private
-
-  def noko
-    @noko ||= Nokogiri::HTML(open(@url).read)
-  end
-
-  def results_box
-    # I can't get the regexp right here to split on \n\s+\n, so this
-    # will have to do for now
-    @_results ||= noko.css('.articleContent p').text.gsub(/\n\s+/m, '----EO----').split('----EO----').map(&:tidy)
-  end
-
-  def parse_winners(str)
-    str.split(/(?:, | and )/).map { |s|
-      s.reverse.split(/\s+/,2).reverse.map(&:reverse)
-    }.map { |name, votes| { name: name, votes: votes.sub(/\.$/,'') } }
-  end
-
 end
 
 source = 'http://www.naurugov.nr/government-information-office/media-release/elected-members-for-nauru-elections-2016-(1).aspx'
-data = Page.new(source).as_data[:results].map do |r|
+data = NauruGov::ElectionResults.new(source).as_data[:results].map do |r|
   r[:winners].map do |w|
     w.merge(area: r[:area])
   end
