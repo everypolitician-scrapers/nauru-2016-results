d60a3d045ba13ddfbeeedfa37a5c961c29e3ba00
Tony Bowden <tony@tmtm.com>
2017-01-07 11:04:22 +0000

    Rewrite to use Scraped
    Simplistic port of the existing data. TODO: tidy this up with a fragment
    class.

--- scraper.rb
+++ scraper.rb
@@ -2,74 +2,45 @@
 # encoding: utf-8
 # frozen_string_literal: true
 
-require 'nokogiri'
-require 'open-uri'
 require 'pry'
+require 'scraped'
 require 'scraperwiki'
 
 require 'open-uri/cached'
 OpenURI::Cache.cache_path = '.cache'
 
-class String
-  def tidy
-    gsub(/[[:space:]]+/, ' ').strip
-  end
-end
-
-class Polidata
-  class Page
-    def initialize(url)
-      @url = url
-    end
-
-    def as_data
-      @md ||= Hash[protected_methods.map { |m| [m, send(m)] }]
-    end
-
-    private
-
-    def noko
-      @noko ||= Nokogiri::HTML(open(@url).read)
+class ResultsPage < Scraped::HTML
+  field :results do
+    triples = results_box[results_box.find_index('Yaren')..-2]
+    triples.each_slice(3).map do |area, winners, _|
+      {
+        area:    area,
+        winners: parse_winners(winners),
+      }
     end
   end
-end
 
-class NauruGov
-  class ElectionResults < Polidata::Page
-    protected
+  private
 
-    def results
-      triples = results_box[results_box.find_index('Yaren')..-2]
-      triples.each_slice(3).map do |area, winners, _|
-        {
-          area:    area,
-          winners: parse_winners(winners),
-        }
-      end
-    end
+  def results_box
+    # I can't get the regexp right here to split on \n\s+\n, so this
+    # will have to do for now
+    @_results ||= noko.css('.articleContent p').text.gsub(/\n\s+/m, '----EO----').split('----EO----').map(&:tidy)
+  end
 
-    private
-
-    def results_box
-      # I can't get the regexp right here to split on \n\s+\n, so this
-      # will have to do for now
-      @_results ||= noko.css('.articleContent p').text.gsub(/\n\s+/m, '----EO----').split('----EO----').map(&:tidy)
-    end
-
-    def parse_winners(str)
-      str.split(/(?:, | and )/).map do |s|
-        s.reverse.split(/\s+/, 2).reverse.map(&:reverse)
-      end.map { |name, votes| { name: name, votes: votes.sub(/\.$/, '') } }
-    end
+  def parse_winners(str)
+    str.split(/(?:, | and )/).map do |s|
+      s.reverse.split(/\s+/, 2).reverse.map(&:reverse)
+    end.map { |name, votes| { name: name, votes: votes.sub(/\.$/, '') } }
   end
 end
 
 source = 'http://www.naurugov.nr/government-information-office/media-release/elected-members-for-nauru-elections-2016-(1).aspx'
-data = NauruGov::ElectionResults.new(source).as_data[:results].map do |r|
+data = ResultsPage.new(response: Scraped::Request.new(url: source).response).results.map do |r|
   r[:winners].map do |w|
     w.merge(area: r[:area])
   end
 end.flatten
 
-warn data
+# puts data
 ScraperWiki.save_sqlite(%i(name area), data)
